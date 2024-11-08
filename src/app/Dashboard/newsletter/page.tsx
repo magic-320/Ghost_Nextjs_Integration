a535838a874e0e8e59b0bc59d0390a65cacc7e31
web-talent <oleksiikaravanov@gmail.com>
2024-11-08 09:16:15 -0800

    update newsletter

--- src/app/Dashboard/newsletter/page.tsx
+++ src/app/Dashboard/newsletter/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 
-import Newsletter from "./[Newsletter]"; // Adjust the path as needed
+// import Newsletter from "./Newsletter"; // Adjust the path as needed
 
-export default function DashboardNewsletterPage() {
-    return <Newsletter />;
-}
+// export default function DashboardNewsletterPage() {
+//     return <Newsletter />;
+// }
+
+import dynamic from "next/dynamic";
+
+const Newsletter = dynamic(() => import('./Newsletter'), { ssr: false });
+
+export default Newsletter;
