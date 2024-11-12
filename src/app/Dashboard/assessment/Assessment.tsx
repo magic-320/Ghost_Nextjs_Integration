2c380d32465d86216eb2add09adf8d524c6c5715
web-talent <oleksiikaravanov@gmail.com>
2024-11-12 03:25:35 -0800

    update chatbot

--- src/app/Dashboard/assessment/Assessment.tsx
+++ src/app/Dashboard/assessment/Assessment.tsx
@@ -1,14 +1,31 @@
 'use client';
 
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { FaStar } from "react-icons/fa";
 
 const Assessment: FC = () => {
+
+    const [rating, setRating] = useState<number>(3);
+
+    React.useEffect(() => {
+
+    }, [])
+
     return (
         <div className='w-full h-full bg-[#F9F9F9] rounded-[22px] px-7 py-10'>
             <div className='w-full h-[600px] bg-[#FFF] rounded-[22px]'>
-                <h1 className='text-[24px] font-bold font-inter font-[#344054] px-7 py-5 text-text-color'>
+                <h1 className='text-[24px] font-bold font-inter font-[#344054] px-7 pt-5 text-text-color'>
                     Assessment
                 </h1>
+                <div className='mx-3 my-2'>
+                    <div className='flex gap-2'>
+                        <FaStar size={35} color='#aaa'/>
+                        <FaStar size={35} color='#aaa'/>
+                        <FaStar size={35} color='#aaa'/>
+                        <FaStar size={35} color='#aaa'/>
+                        <FaStar size={35} color='#aaa'/>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/app/Dashboard/chatbot/Chatbot.tsx
+++ src/app/Dashboard/chatbot/Chatbot.tsx
@@ -1,18 +1,85 @@
 'use client';
 
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import Image from 'next/image';
+import parse from 'html-react-parser';
 import Edosa_mark from '@/public/assets/images/O_edosa.png';
 
 const Chatbot: FC = () => {
+
+    const [data, setData] = useState<any[]>([
+        {
+            isMe: true,
+            content: "Hello, My name is Edosa Odaro and I can .... aaaaa bbbbb cccc ddddd eeee fffff ggggg"
+        },
+        {
+            isMe: false,
+            content: "Hello, I and I can .... aaaaa bbbbb cccc ddddd eeee fffff ggggg ddddd cccccc vvvvvv"
+        },
+        {
+            isMe: true,
+            content: "Hello, My name is Edosa Odaro"
+        },
+        {
+            isMe: false,
+            content: "Hello, I and I can .... aaaaa bbbbb cccc ddddd eeee fffff ggggg ddddd cccccc vvvvvv"
+        },
+        {
+            isMe: true,
+            content: "Hello, My name is Edosa Odaro and I can .... aaaaa bbbbb cccc ddddd eeee fffff ggggg"
+        },
+        {
+            isMe: false,
+            content: "Hello, I and I can .... aaaaa bbbbb cccc ddddd eeee fffff ggggg ddddd cccccc vvvvvv"
+        },
+        {
+            isMe: true,
+            content: "Hello, My name is Edosa Odaro and I can .... aaaaa bbbbb cccc ddddd eeee fffff ggggg"
+        },
+        {
+            isMe: false,
+            content: "Hello, I and I can .... aaaaa bbbbb cccc ddddd eeee fffff ggggg ddddd cccccc vvvvvv"
+        }
+    ]);
+
+    React.useEffect(() => {
+        console.log('================== Effect =================')
+    }, [])
+
     return (
         <div className='w-full h-full bg-[#F9F9F9] rounded-[22px] px-7 py-10'>
             <div className='relative w-full h-[650px] bg-[#FFF] rounded-[22px]'>
                 <h1 className='text-[24px] font-bold font-inter font-[#344054] px-7 py-5 text-text-color'>
                     My Chatbot
                 </h1>
-                <div>
-
+                <div className='overflow-auto h-[470px]'>
+                    {
+                        data.map((el: any, index:number) => {
+                            return el.isMe ? (
+                                <div key={index} className="mx-5 my-3 flex flex-row-reverse">
+                                    <div className="w-[40px] aspect-[1/1] border-2 border-solid border-[#F15F31] font-[MuseoModerno] font-bold text-[#344054] rounded-full flex justify-center items-center self-end mb-[8px]">
+                                        <Image 
+                                            className="w-5" 
+                                            alt={'edosa_mark'} 
+                                            src={Edosa_mark} 
+                                        />
+                                    </div>
+                                    <div className="mx-3 p-4 w-auto max-w-[550px] h-auto bg-[#FFF7F4] font-[#344054] font-inter rounded-[22px]">
+                                        {parse(el.content)}
+                                    </div>
+                                </div>
+                            ) : (
+                                <div key={index} className="mx-5 my-3 flex">
+                                    <div className="w-[40px] aspect-[1/1] border-2 border-solid border-[#344054] font-[MuseoModerno] font-bold text-[#344054] rounded-full flex justify-center items-center self-end mb-[8px]">
+                                        JD
+                                    </div>
+                                    <div className="mx-3 p-4 w-auto max-w-[550px] h-auto bg-[#F4F4F4] font-[#344054] font-inter rounded-[22px]">
+                                        {parse(el.content)}
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
                 <div className='absolute bottom-5 w-full px-10'>
                     <div className="relative w-full">
