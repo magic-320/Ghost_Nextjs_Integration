cb5d7eb7b6d846ac868ebdd68ad64da1249bf9e9
web-talent <oleksiikaravanov@gmail.com>
2024-12-13 08:13:13 -0800

    update sidebar

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,22 +1,40 @@
 'use client'
-import React, { FC, useState } from "react";
+import React, { Dispatch, FC, SetStateAction, useState } from "react";
 import { IoIosArrowDown, IoIosArrowUp  } from "react-icons/io";
 import Link from "next/link";
 import { usePathname } from 'next/navigation';
 import axios from "axios";
 
 
-const Sidebar: FC = () => {
+interface SidebarProps {
+    onCollapse?: boolean; // Define this based on your needs
+}
+
+const items = {
+    dashboard: 'My Dashboard',
+    learn: 'Learn',
+    read: 'Read',
+    watch: 'Watch',
+    meet: 'Meet',
+    chatbot: 'My Chatbot',
+    newsletter: 'My Newsletter',
+    assessment: 'Assessment'
+}
+
+function Sidebar( { onCollapse }: SidebarProps ) {
 
     const [active, setActive] = useState<string>('');
     const [serviceHidden, setServiceHiden] = useState<boolean>(false);
     const [isChatbot, setIsChatbot] = useState<boolean>(true);
+    const [expand, setExpand] = useState<boolean>();
 
     const pathname = usePathname();
 
     React.useEffect(() => {
-        
-        switch(pathname) {
+
+        onCollapse ? setExpand(false) : setExpand(true);
+
+        switch (pathname) {
             case "/Dashboard":
                 setActive('dashboard');
                 break;
@@ -32,6 +50,9 @@ const Sidebar: FC = () => {
             case "/Dashboard/meet":
                 setActive('meet');
                 break;
+            case "/Dashboard/chatbot":
+                setActive('chatbot');
+                break;
             case "/Dashboard/newsletter":
                 setActive('newsletter');
                 break;
@@ -44,98 +65,134 @@ const Sidebar: FC = () => {
 
 
         // get Chatbot available status
-        const getChatbotAvailable = async() => {
+        const getChatbotAvailable = async () => {
             const response = await axios.get<any>('/api/chatbotAdmin/getChatbot');
             setIsChatbot(response.data.enable);
-        }
+        };
 
         getChatbotAvailable();
 
-    }, [])
+    }, []);
 
     return (
-        <div className="w-full h-full py-10 text-[17px] xl:text-[22px] bg-[#F9F9F9] rounded-[22px] lg:block flex justify-center">
-            <div className="text-center lg:text-left mx-[auto] max-w-[125px] xl:max-w-[155px]">
-                <Link href="/Dashboard">
+        <div className="w-full h-full py-3 lg:py-10 text-[17px] xl:text-[22px] bg-[#F9F9F9] rounded-[22px] lg:block">
+            {
+                onCollapse && (
                     <div 
-                        className={`hover:cursor-pointer ${active == 'dashboard' && 'font-bold'}`} 
-                        onClick={() => setActive('dashboard')}
+                        className="text-center lg:text-left mx-[auto] font-bold text-[20px] text-text-color hover:cursor-pointer"
+                        onClick={() => setExpand(!expand)}
                     >
-                        My Dashboard
+                        {items[active as keyof typeof items]}
                     </div>
-                </Link>
-                <div 
-                    className="mt-4 hover:cursor-pointer flex" 
-                    onClick={() => setServiceHiden(!serviceHidden)}
-                >
-                    My Services <span className="ml-2 md:ml-3 mt-1 md:mt-2">{serviceHidden ? <IoIosArrowDown /> : <IoIosArrowUp />}</span>
-                </div>
-                    <div className={`px-10 ${serviceHidden && 'hidden'}`}>
-                        <Link href="/Dashboard/learn">
-                            <div 
-                                className={`mt-4 hover:cursor-pointer ${active == 'learn' && 'font-bold'}`}
-                                onClick={() => setActive('learn')}
+                )
+            }
+            {
+                expand && (
+                    <div className="text-center lg:text-left mx-[auto] max-w-[125px] xl:max-w-[155px] mt-5 lg:mt-0">
+                        <Link href="/Dashboard">
+                            <div
+                                className={`hover:cursor-pointer ${active == 'dashboard' && 'font-bold'}`}
+                                onClick={() => {
+                                    setActive('dashboard');
+                                    onCollapse && setExpand(false);
+                                }}
                             >
-                                Learn
+                                My Dashboard
                             </div>
                         </Link>
-                        <Link href="/Dashboard/read">
-                            <div 
-                                className={`mt-4 hover:cursor-pointer ${active == 'read' && 'font-bold'}`}
-                                onClick={() => setActive('read')}
+                        <div
+                            className="mt-2 lg:mt-4 hover:cursor-pointer flex"
+                            onClick={() => setServiceHiden(!serviceHidden)}
+                        >
+                            My Services <span className="ml-2 md:ml-3 mt-1 md:mt-2">{serviceHidden ? <IoIosArrowDown /> : <IoIosArrowUp />}</span>
+                        </div>
+                        <div className={`px-10 ${serviceHidden && 'hidden'}`}>
+                            <Link href="/Dashboard/learn">
+                                <div
+                                    className={`mt-2 lg:mt-4 hover:cursor-pointer ${active == 'learn' && 'font-bold'}`}
+                                    onClick={() => {
+                                        setActive('learn');
+                                        onCollapse && setExpand(false);
+                                    }}
+                                >
+                                    Learn
+                                </div>
+                            </Link>
+                            <Link href="/Dashboard/read">
+                                <div
+                                    className={`mt-2 lg:mt-4 hover:cursor-pointer ${active == 'read' && 'font-bold'}`}
+                                    onClick={() => {
+                                        setActive('read');
+                                        onCollapse && setExpand(false);
+                                    }}
+                                >
+                                    Read
+                                </div>
+                            </Link>
+                            <Link href="/Dashboard/watch">
+                                <div
+                                    className={`mt-2 lg:mt-4 hover:cursor-pointer ${active == 'watch' && 'font-bold'}`}
+                                    onClick={() => {
+                                        setActive('watch');
+                                        onCollapse && setExpand(false);
+                                    }}
+                                >
+                                    Watch
+                                </div>
+                            </Link>
+                            <Link href="/Dashboard/meet">
+                                <div
+                                    className={`mt-2 lg:mt-4 hover:cursor-pointer ${active == 'meet' && 'font-bold'}`}
+                                    onClick={() => {
+                                        setActive('meet');
+                                        onCollapse && setExpand(false);
+                                    }}
+                                >
+                                    Meet
+                                </div>
+                            </Link>
+                        </div>
+                        {isChatbot && (
+                            <Link href="/Dashboard/chatbot">
+                                <div
+                                    className={`mt-2 lg:mt-4 hover:cursor-pointer ${active == 'chatbot' && 'font-bold'}`}
+                                    onClick={() => {
+                                        setActive('chatbot');
+                                        onCollapse && setExpand(false);
+                                    }}
+                                >
+                                    My Chatbot
+                                </div>
+                            </Link>
+                        )}
+                        <Link href="/Dashboard/newsletter">
+                            <div
+                                className={`mt-2 lg:mt-4 hover:cursor-pointer ${active == 'newsletter' && 'font-bold'}`}
+                                onClick={() => {
+                                    setActive('newsletter');
+                                    onCollapse && setExpand(false);
+                                }}
                             >
-                                Read
+                                My Newsletter
                             </div>
                         </Link>
-                        <Link href="/Dashboard/watch">
-                            <div 
-                                className={`mt-4 hover:cursor-pointer ${active == 'watch' && 'font-bold'}`}
-                                onClick={() => setActive('watch')}
+                        <Link href="/Dashboard/assessment">
+                            <div
+                                className={`mt-2 lg:mt-4 hover:cursor-pointer ${active == 'assessment' && 'font-bold'}`}
+                                onClick={() => {
+                                    setActive('assessment');
+                                    onCollapse && setExpand(false);
+                                }}
                             >
-                                Watch
+                                Assessment
                             </div>
                         </Link>
-                        <Link href="/Dashboard/meet">
-                            <div 
-                                className={`mt-4 hover:cursor-pointer ${active == 'meet' && 'font-bold'}`}
-                                onClick={() => setActive('meet')}
-                            >
-                                Meet
-                            </div>
-                        </Link>
-                    </div>
-                {
-                    isChatbot && (
-                        <Link href="/Dashboard/chatbot">
-                            <div 
-                                className={`mt-4 hover:cursor-pointer ${active == 'chatbot' && 'font-bold'}`}
-                                onClick={() => setActive('chatbot')}
-                            >
-                                My Chatbot
-                            </div>
-                        </Link>
-                    )
-                }
-                <Link href="/Dashboard/newsletter">
-                    <div 
-                        className={`mt-4 hover:cursor-pointer ${active == 'newsletter' && 'font-bold'}`}
-                        onClick={() => setActive('newsletter')}
-                    >
-                        My Newsletter
-                    </div>
-                </Link>
-                <Link href="/Dashboard/assessment">
-                    <div 
-                        className={`mt-4 hover:cursor-pointer ${active == 'assessment' && 'font-bold'}`}
-                        onClick={() => setActive('assessment')}
-                    >
-                        Assessment
-                    </div>
-                </Link>
 
-            </div>
+                    </div>
+                )
+            }
         </div>
-    )
+    );
 }
 
 export default Sidebar;
